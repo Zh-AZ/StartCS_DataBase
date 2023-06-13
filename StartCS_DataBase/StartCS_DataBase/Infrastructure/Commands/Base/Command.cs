36f9dbfbcd0a67ec5fee8500f9c3a7f3eec7c5a8
Isaac <zhaparovaziz03@gmail.com>
2023-06-13 15:17:27 +0300

    Подключение к (localdb)\MSSQLLocalDB

---
+++ StartCS_DataBase/StartCS_DataBase/Infrastructure/Commands/Base/Command.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace StartCS_DataBase.Infrastructure.Commands.Base
+{
+    internal abstract class Command
+    {
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        public abstract bool CanExecute(object parameter);
+        public abstract void Execute(object parameter);
+    }
+}
---
+++ StartCS_DataBase/StartCS_DataBase/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using StartCS_DataBase.Infrastructure.Commands.Base;
+using StartCS_DataBase.Infrastructure.Commands;
+using StartCS_DataBase.ViewModels.Base;
+using System.Windows.Markup;
+using System.Data.SqlClient;
+
+namespace StartCS_DataBase.ViewModels
+{
+    internal class MainWindowViewModel : ViewModel
+    {
+        public MainWindowViewModel()
+        {
+        }
+
+        //Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = MSSQLLocalDemo; Integrated Security = True; Pooling=False
+
+        SqlConnectionStringBuilder sqlConnectionStringBuilder = new SqlConnectionStringBuilder()
+        {
+            DataSource = "(localdb)\\MSSQLLocalDB",
+            InitialCatalog = "MSSQLLocalDemo",
+            IntegratedSecurity = true,
+            UserID = "Admin", Password = "qwerty",
+            Pooling = true
+        };
+    }
+}
