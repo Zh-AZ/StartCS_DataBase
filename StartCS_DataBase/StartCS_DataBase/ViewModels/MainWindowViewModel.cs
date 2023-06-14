9cb4970046b0a27be414a22878d58e9f5b3e4bde
Isaac <zhaparovaziz03@gmail.com>
2023-06-14 19:28:08 +0300

    Работа с БД в C#

--- StartCS_DataBase/StartCS_DataBase/ViewModels/MainWindowViewModel.cs
+++ StartCS_DataBase/StartCS_DataBase/ViewModels/MainWindowViewModel.cs
@@ -8,6 +8,7 @@ using StartCS_DataBase.Infrastructure.Commands;
 using StartCS_DataBase.ViewModels.Base;
 using System.Windows.Markup;
 using System.Data.SqlClient;
+using System.Windows;
 
 namespace StartCS_DataBase.ViewModels
 {
@@ -15,6 +16,8 @@ namespace StartCS_DataBase.ViewModels
     {
         public MainWindowViewModel()
         {
+            ConnectionBD();
+            WriteToBD();
         }
 
         //Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = MSSQLLocalDemo; Integrated Security = True; Pooling=False
@@ -27,5 +30,81 @@ namespace StartCS_DataBase.ViewModels
             UserID = "Admin", Password = "qwerty",
             Pooling = true
         };
+
+        void ConnectionBD()
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString))
+                {
+                    sqlConnection.Open();
+                    string sqlServer = @"SELECT 
+                        Clients.clients,
+                        Managers.managers,
+                        TempTable.id as 'ID_Client'
+                        FROM Clients, Managers, TempTable
+                        WHERE Clients.id = TempTable.id or Managers.id = TempTable.id";
+                    
+                    SqlCommand sqlCommand = new SqlCommand(sqlServer, sqlConnection);
+                    SqlDataReader sqlDataReader = sqlCommand.ExecuteReader();
+
+                    while (sqlDataReader.Read()) 
+                    {
+                        /*Do something*/
+                        Console.WriteLine($"{sqlDataReader[0]} | " +
+                                          $"{sqlDataReader[1]} | " +
+                                          $"{sqlDataReader[2]} | "); 
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
+
+        void WriteToBD()
+        {
+            try
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(sqlConnectionStringBuilder.ConnectionString))
+                {
+                    sqlConnection.Open();
+
+                    string[] sqlWriteToBD =
+                    {
+                        "INSERT INTO Clients ([id],[clients], [values]) values (04, 'Client_04', 'Val_04')",
+                        "INSERT INTO Managers ([id],[managers], [description]) values (03, 'Manager_03', 'Des_03')",
+                    };
+
+                    SqlCommand sqlCommand;
+                    foreach (string sqlDataReader in sqlWriteToBD)
+                    {
+                        sqlCommand = new SqlCommand(sqlDataReader, sqlConnection);
+                        sqlCommand.ExecuteNonQuery();
+
+                        //Изменение значения
+                        //sqlDataReader = @"UPDATE TempTable SET title = N'Новое поле' WHERE id = 19";
+                        //sqlCommand = new SqlCommand(sqlDataReader, sqlConnection);
+                        //sqlCommand.ExecuteNonQuery();
+
+                        //Удаление указанных данных
+                        //sqlDataReader = @"DELETE FROM TempTable WHERE id > 15";
+                        //sqlCommand = new SqlCommand(sqlDataReader, sqlConnection);
+                        //sqlCommand.ExecuteNonQuery();
+
+                        //Удаление таблицы
+                        //sqlDataReader = @"DROP TABLE TempTable";
+                        //sqlCommand = new SqlCommand(sqlDataReader, sqlConnection);
+                        //sqlCommand.ExecuteNonQuery();
+                        //break;
+                    }
+                }
+            }
+            catch (Exception ex )
+            {
+                MessageBox.Show($"{ex.Message}");
+            }
+        }
     }
 }
